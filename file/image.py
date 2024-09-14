1f5f9e2505343c6be0cebbe3fee21cf95cafb4f4
ziazia919 <ziar6518@gmail.com>
2024-09-15 01:44:05 +0800

    Create image.py

---
+++ file/image.py
@@ -0,0 +1,18 @@
+import cv2
+
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    
+    ret, frame = cap.read()
+    
+    # Display the resulting frame
+    cv2.imshow('Webcam Video', frame)
+    
+    
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
